7207f2f21182fbdc8842e2efe89654a24e2670c5
Nikunj1810 <Garachhnikunj2004@gmail.com>
2025-03-23 09:27:06 +0530

    Initial commit

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.netcore_Project.Controllers
+{
+    public class AccountController : Controller
+    {
+        // Loads the Login View
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        // Loads the Register View
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        // Optional - Redirect to Login or Register as default
+        public IActionResult Index()
+        {
+            return RedirectToAction("Login");
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+using System.Diagnostics;
+using ASP.netcore_Project.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.netcore_Project.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
